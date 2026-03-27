c0e71049b79985546f5d4b1f0edd2c09a3598ea0
Ulises Campuzano Hernandez <20243ds023@utez.edu.mx>
2026-03-26 23:36:22 -0600

    Configurar variable de entorno para API

--- backend/src/main/java/com/noisync/backend/BackendApplication.java
+++ backend/src/main/java/com/noisync/backend/BackendApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.setProperty("TNS_ADMIN", 
+            new java.io.File("Wallet_NOIZYNC679").getAbsolutePath());
+        
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
 
 }
